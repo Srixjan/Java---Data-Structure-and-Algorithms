f13b16d1902260dcfc9e37b6cb398cb57ecbe4c5
Srixjan <srijan.chowdhury2020@gmail.com>
2026-08-29 16:18:52 +0530

    dutch algorithm

---
+++ sortColour.java
@@ -0,0 +1,35 @@
+public class sortColour {
+    public void sortZeroOneTwo(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while(mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 2, 1, 0};
+        
+        sortColour sort = new sortColour();
+        
+        sort.sortZeroOneTwo(nums);
+        
+        System.out.println("After sorting:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
