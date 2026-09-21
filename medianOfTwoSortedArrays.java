b0fe6d57f0d25c0aebed10236640a71292e23b77
Srixjan <srijan.chowdhury2020@gmail.com>
2026-09-21 23:32:11 +0530

    Add KthElementOfTwoSortedArrays class with logic to find the k-th element in two sorted arrays

--- medianOfTwoSortedArrays.java
+++ medianOfTwoSortedArrays.java
@@ -21,7 +21,7 @@ public class medianOfTwoSortedArrays {
             else if(l1 > r2) {
                 high = mid1 - 1;
             } else {
-                low = mid1 + 1;
+                low = mid + 1;
             }
         }
         return 0;
